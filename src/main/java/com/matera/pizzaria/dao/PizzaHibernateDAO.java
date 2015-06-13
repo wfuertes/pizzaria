b98cda2c2a4434f110fec36f5361257ae238a344
Willian Batista <wbatista@kenzanmedia.com>
2015-06-13 14:02:03 -0300

    DIA_03 configuração inicial;

--- src/main/java/com/matera/pizzaria/dao/PizzaHibernateDAO.java
+++ src/main/java/com/matera/pizzaria/dao/PizzaHibernateDAO.java
@@ -1,65 +1,11 @@
 package com.matera.pizzaria.dao;
 
-import com.matera.pizzaria.model.Pizza;
-
-import java.util.List;
-
-import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 /**
  *
  *
  * @author wbatista
  */
-@Repository
-@Transactional
-public class PizzaHibernateDAO implements PizzaDAO {
+public class PizzaHibernateDAO {
 
-    private final SessionFactory factory;
-
-    @Autowired
-    public PizzaHibernateDAO(final SessionFactory factory) {
-
-        this.factory = factory;
-    }
-
-    @Override
-    public Pizza save(Pizza pizza) {
-
-        Long id = (Long) factory.getCurrentSession().save(pizza);
-        pizza.setId(id);
-        return pizza;
-    }
-
-    @Override
-    public Pizza update(Pizza pizza) {
-
-        factory.getCurrentSession().update(pizza);
-        return pizza;
-    }
-
-    @Override
-    public void delete(Long id) {
-
-        Pizza pizza = new Pizza();
-        pizza.setId(id);
-        factory.getCurrentSession().delete(pizza);
-    }
-
-    @Override
-    @Transactional(readOnly = true)
-    public Pizza findOne(Long id) {
-
-        return (Pizza) factory.getCurrentSession().load(Pizza.class, id);
-    }
-
-    @Override
-    @Transactional(readOnly = true)
-    public List<Pizza> findAll() {
-
-        return factory.getCurrentSession().createCriteria(Pizza.class).list();
-    }
 }
--- src/main/java/com/matera/pizzaria/dao/PizzaJpaDAO.java
+++ src/main/java/com/matera/pizzaria/dao/PizzaJpaDAO.java
@@ -1,63 +1,12 @@
 package com.matera.pizzaria.dao;
 
-import com.matera.pizzaria.model.Pizza;
-
-import java.util.List;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
-import org.springframework.context.annotation.Primary;
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 /**
  *
  *
  * @author wbatista
  */
-@Primary
-@Repository
-@Transactional
-public class PizzaJpaDAO implements PizzaDAO {
+public class PizzaJpaDAO {
 
-    private EntityManager em;
-
-    @PersistenceContext
-    public void setEntityManager(final EntityManager em) {
-
-        this.em = em;
-    }
-
-    @Override
-    public Pizza save(Pizza pizza) {
-
-        return em.merge(pizza);
-    }
-
-    @Override
-    public Pizza update(Pizza pizza) {
-
-        return save(pizza);
-    }
-
-    @Override
-    public void delete(Long id) {
-
-        em.createQuery("delete from Pizza where id = :id").setParameter("id", id).executeUpdate();
-    }
-
-    @Override
-    @Transactional(readOnly = true)
-    public Pizza findOne(Long id) {
-
-        return em.find(Pizza.class, id);
-    }
-
-    @Override
-    @Transactional(readOnly = true)
-    public List<Pizza> findAll() {
-
-        return em.createQuery("from Pizza order by name", Pizza.class).getResultList();
-    }
+    
 }
